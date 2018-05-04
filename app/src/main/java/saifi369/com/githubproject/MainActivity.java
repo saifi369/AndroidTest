f04c1009055d5d07c307ab26bf82be9c18a68eff
saifi369 <chsaif369@gmail.com>
2018-05-05 02:22:47 +0500

    Initial commit

---
+++ app/src/main/java/saifi369/com/githubproject/MainActivity.java
@@ -0,0 +1,15 @@
+package saifi369.com.githubproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //This is brand new project
+    }
+}
